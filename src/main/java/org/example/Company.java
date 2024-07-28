243aee3608658e0d54704b8fbb39fd8622d71362
arife-senol <avcisenolarife@gmail.com>
2024-07-29 01:01:01 +0300

    Sprint challenge solved

---
+++ src/main/java/org/example/Company.java
@@ -0,0 +1,78 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Company {
+    private int id;
+    private String name;
+
+    private double giro;
+    private String[] developerNames;
+
+    public Company(int id, String name, double giro, String[] developerNames) {
+        this.id = id;
+        this.name = name;
+        this.giro = giro;
+        this.developerNames = developerNames;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getGiro() {
+        return giro;
+    }
+
+    public void setGiro(double giro) {
+        if(giro>=0) {
+            this.giro = giro;
+        }
+    }
+
+    public String[] getDeveloperNames() {
+        return developerNames;
+    }
+
+    public void setDeveloperNames(String[] developerNames) {
+        this.developerNames = developerNames;
+    }
+    public boolean addEmployee(int index, String name)
+    {
+        if(developerNames.length-1>=index && index>=0 && developerNames != null) {
+            if (developerNames[index] == null) {
+                developerNames[index] = name;
+                return true;
+            } else {
+                System.out.println("Bu alan dolu");
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", giro=" + giro +
+                ", developerNames=" + Arrays.toString(developerNames) +
+                '}';
+    }
+}
